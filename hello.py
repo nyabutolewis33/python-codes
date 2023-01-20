b3003afbfd6dfe2bd6afd5ed5cad4743b976ee3b
nyabutolewis33 <109216207+nyabutolewis33@users.noreply.github.com>
2023-01-20 08:47:38 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,19 @@
+"""
+My first program in python
+"""
+#string -comment
+print('Hello, world!')
+
+name = "python"
+print(name)
+
+#boolean
+print(True)
+
+#integer
+sum = 20 +5
+print(sum)
+
+#floating point number
+decimal = 2.321 + 6.021
+print(decimal)
